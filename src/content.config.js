b6ecc627405153baa4eb9f6e5d01d8a7d6b5c713
C1cerius <email@domain.com>
2026-01-21 17:31:50 +0200

    add test second post to check, add toggling sidebar functionality

--- src/content.config.js
+++ src/content.config.js
@@ -4,7 +4,9 @@ import { glob } from "astro/loaders";
 const posts = defineCollection({
 	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/data/blog-posts" }),
 	schema: z.object({
+		postMain: z.boolean(),
 		title: z.string(),
+		parentSlug: z.string().optional(),
 		slug: z.string(),
 		publishDate: z.union([z.string(), z.date()]),
 		description: z.string(),
